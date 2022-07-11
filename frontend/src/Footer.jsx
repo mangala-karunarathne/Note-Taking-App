933f646df87df1cdf3fb8593abef253f4705b0be
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-11 07:42:00 -0700

    Modifications - Footer, Note Heading

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
+const curryear = new Date().getFullYear();
 
-function Notes() {
+function Footer() {
   return (
-    <div className="note">
-      <h1>This the Heading of the Note</h1>
-      <p>Place your own  note here and do your works smartly. Don't miss anything</p>     
-    </div>
+    <footer>
+      <p> Coppyright @ The Civil Today {curryear} </p>
+    </footer>
   );
-}a
+}
 
-export default Notes;
+export default Footer;
--- frontend/src/Notes.jsx
+++ frontend/src/Notes.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Notes() {
   return (
     <div className="note">
-      <h1>This the Heading of the Note</h1>
+      <h1>This is the Heading of the Note</h1>
       <p>Place your own  note here and do your works smartly. Don't miss anything</p>     
     </div>
   );
