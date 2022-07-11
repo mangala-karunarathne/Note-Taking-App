3ace561352fa68bd9dd88dd59a34be7c1ec2839c
mangala-karunarathne <mangalaicc@gmail.com>
2022-07-10 21:22:35 -0700

    Footer and Notes

---
+++ frontend/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This the Heading of the Note</h1>
+      <p>Place your own  note here and do your works smartly. Don't miss anything</p>     
+    </div>
+  );
+}a
+
+export default Notes;
